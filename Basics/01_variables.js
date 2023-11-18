fcc44115232a2cfd5d6b71875382253ebd7ecaea
Himanshu <134915268+himan97@users.noreply.github.com>
2023-11-18 10:42:37 +0000

    Deleted Basics file.

--- Basics/01_variables.js
+++
@@ -1,8 +0,0 @@
-const acId = 1234 ;
-let acName = "Himanshu";
-var acEmail = "xyz@email.com";
-
-
-//Please don't Use 'var';
-
-console.log(acId);
--- Basics/02_boolean.js
+++
@@ -1,3 +0,0 @@
-let number = 26;
-let changeNumToboolean = Boolean(number);
-console.log(changeNumToboolean);
