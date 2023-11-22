c78f2fd07132ac4874c2843cc8a51ab8cad11256
Himanshu <134915268+himan97@users.noreply.github.com>
2023-11-22 11:20:46 +0000

    added switchCase and ifElse files...

---
+++ intermediate/ifelse.js
@@ -0,0 +1,9 @@
+
+if(true) console.log("exucuted");
+
+if (2==3) console.log(true) ;else console.log(false);
+
+// We can write if and else statements in single line like I have written in above lines.
+
+
+    
